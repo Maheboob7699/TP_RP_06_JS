ad2c320f6aa52852910e87d5518af9c6af392a89
Maheboob7699 <murdimaheboob@gmail.com>
2024-05-02 05:17:51 -0900

    function

---
+++ index.js
@@ -0,0 +1,36 @@
+function fullName( name){
+    console.log("hello",name);
+    }
+    
+    fullName("Mehboob")
+    
+    
+    
+    
+    // Area of Rectangle
+    
+    function areaofRectangle(length,breath){
+        let Area=length*breath;
+        console.log("Area of Rectangle is ",Area);
+    }
+    areaofRectangle(23,43)
+    
+    
+    
+    
+    //  clesius to  Fahrenheit conversion
+    function CelsiustoFahrenheit(celsius) {
+        let FahrenheitTemperature =(9/5)*celsius + 32;
+        console.log("celsiusis" ,FahrenheitTemperature);
+    }
+    CelsiustoFahrenheit(30);
+    
+    
+    
+    
+    //  fahrenheit to celsius conversion
+    function FahrenheittoCelsius(fahrenheit) {
+        let celsiusTemperature =(fahrenheit-32)*(5/9);
+        console.log("fahrenheit is" , celsiusTemperature);
+    }
+    FahrenheittoCelsius (40);
