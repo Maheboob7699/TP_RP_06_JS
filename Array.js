ced174f7baab47ebfeb57392ec7366679d688671
Maheboob7699 <murdimaheboob@gmail.com>
2024-05-06 20:35:09 +0530

    Array

---
+++ Array.js
@@ -0,0 +1,17 @@
+var fullName=["MEhboob","Anuj","Dipu","Musharaf"];
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName[3]);
+fullName.push("Kasim");
+console.log(fullName);
+
+
+
+var Netflix=["Chamkila","Money heist","Asur","Apharan"];
+console.log(Netflix);
+
+Netflix.push("Badnewz");
+console.log(Netflix);
+
+Netflix.pop("Apharan");
+console.log(Netflix);
