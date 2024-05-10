cd34f29a1b44556f681748736bab4336392668d4
Maheboob7699 <murdimaheboob@gmail.com>
2024-05-10 20:04:11 +0530

    Objec_js

---
+++ Object/object.js
@@ -0,0 +1,18 @@
+// object:- Object ic thr data type where store multiple type of data with different types of data.
+let student={
+    id:134,
+    name:"Mehboob",
+    address:"Ghtakopar",
+    skills:["Html","Css","Js","React"],
+ }
+
+
+
+//  print the value 
+ console.log(student);
+ console.log(student.skills);
+
+
+//  change value
+ student.id=213;
+ console.log(student.id);
